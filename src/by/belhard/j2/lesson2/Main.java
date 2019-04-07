0ec398a229cc62be9e28104177b9014489cb807b
Rozenberg <avangard.npaper@gmail.com>
2019-04-07 17:29:24 +0300

    lesson2 examples

--- src/by/belhard/j2/lesson2/Main.java
+++ src/by/belhard/j2/lesson2/Main.java
@@ -2,9 +2,39 @@ package by.belhard.j2.lesson2;
 
 public class Main {
 
-    int x;
 
     public static void main(String[] args) {
 
+        int x = 23;
+
+        // условный оператор
+        if (x == 20) {
+            x--;
+        } else if (x == 21) {
+            x++;
+        } else {
+            x /= 2;
+        }
+
+        // тернарный условный оператор
+        int y = (x % 2 == 0) ? (x / 2) : (-x);
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // цикл while
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i++);
+        }
+
+        do {
+            i--;
+            System.out.println(i);
+        } while (i >= -2);
+
+        for (int j = 0; j <= 10; j += 2) {
+            System.out.print(j + " ");
+        }
     }
 }
